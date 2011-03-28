2153a5b797c71f626e76d37cae74c6f77c88da96
Wilbur suero <wilbur.himself@gmail.com>
2011-03-28 01:56:10 -0400

    Home page

--- views/categories/list_home.php
+++ views/categories/list_home.php
@@ -1,6 +1,6 @@
 <div class="category-block">
     <h3><?php print $object->name; ?></h3>
     <ul>
-        <?php print partial_collection($object->jobs, 'jobs/item'); ?>
+        <?php print partial_collection($object->jobs, 'jobs/item', array('limit' => 20)); ?>
     </ul>
 </div>
